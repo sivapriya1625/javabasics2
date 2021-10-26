635ea75a671ce7c20b48a298e1b42aa48809e41a
sjammala <sjammala@DESKTOP-LGOLIJ3>
2021-10-26 02:14:22 -0700

    new arrayliat

--- dxcjava/src/collections/Arraylist.java
+++ dxcjava/src/collections/Arraylist.java
@@ -6,10 +6,10 @@ public class Arraylist {
 	
 	public static void main(String args[]){  
 		ArrayList<String> list=new ArrayList<String>();//Creating arraylist  
-		list.add("Ravi");//Adding object in arraylist  
-		list.add("Vijay");  
-		list.add("Ravi");  
-		list.add("Ajay");  
+		list.add("priya");//Adding object in arraylist  
+		list.add("shiva");  
+		list.add("swetha");  
+		list.add("narayana");  
 		//Traversing list through Iterator  
 		Iterator itr=list.iterator();  
 		while(itr.hasNext()){  
