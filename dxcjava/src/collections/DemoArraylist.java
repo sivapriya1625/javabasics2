d91ca2454191749d9c505b5c9032a60f697d0310
sjammala <sjammala@DESKTOP-LGOLIJ3>
2021-10-25 22:36:27 -0700

    arraylist

---
+++ dxcjava/src/collections/DemoArraylist.java
@@ -0,0 +1,16 @@
+package collections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DemoArraylist {
+	
+	public static void main(String[] args) {
+		List<String> names = new ArrayList<String>();
+		names.add("priya");
+		//names.add(30);
+		System.out.println(names);
+
+}
+	
+}
