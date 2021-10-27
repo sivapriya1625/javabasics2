a4ae2675c325630c2c60e32fdf20b42ba98132e0
sjammala <sjammala@DESKTOP-LGOLIJ3>
2021-10-27 09:59:50 -0700

    polymorphism

---
+++ dxcjava/src/polymorphism/Animal.java
@@ -0,0 +1,10 @@
+package polymorphism;
+class Animal{  
+	Animal(){System.out.println("animal is created");}  
+}  
+class Dog extends Animal{  
+	Dog(){  
+		//super();  
+		System.out.println("dog is created");  
+	}  
+}  
---
+++ dxcjava/src/polymorphism/TestSuper3.java
@@ -0,0 +1,8 @@
+package polymorphism;
+
+public class TestSuper3 {
+
+	public static void main(String args[]){  
+		Dog d=new Dog();  
+	}
+}
