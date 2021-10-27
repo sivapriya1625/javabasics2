817a7a28aaf7d222ca1da89fac7a0f92a8c08700
sjammala <sjammala@DESKTOP-LGOLIJ3>
2021-10-26 22:22:07 -0700

    arraylist

--- dxcjava/src/collections/LinkedArraylist.java
+++ dxcjava/src/collections/LinkedArraylist.java
@@ -6,21 +6,18 @@ public class LinkedArraylist {
 	
 	public static void main(String args[]){  
 		  
-		List<String> al=new ArrayList<String>();//creating arraylist    
-		  al.add("Ravi");//adding object in arraylist    
-		  al.add("Vijay");    
-		  al.add("Ravi");    
-		  al.add("Ajay");    
-		    
-		  List<String> al2=new LinkedList<String>();//creating linkedlist    
-		  al2.add("James");//adding object in linkedlist    
-		  al2.add("Serena");    
-		  al2.add("Swati");    
-		  al2.add("Junaid");    
-		    
-		  System.out.println("arraylist: "+al);  
-		  System.out.println("linkedlist: "+al2);  
-		 }    
+		//Creating a List  
+		 List<String> list=new ArrayList<String>();  
+		 //Adding elements in the List  
+		 list.add("Mango");  
+		 list.add("Apple");  
+		 list.add("Banana");  
+		 list.add("Grapes");  
+		 //Iterating the List element using for-each loop  
+		 for(String fruit:list)  
+		  System.out.println(fruit);  
+		  
+		}  
 		 }  
 
 
