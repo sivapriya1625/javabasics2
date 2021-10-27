0b997cf560c11eac826ecf3f89dbf40d99584ff2
sjammala <sjammala@DESKTOP-LGOLIJ3>
2021-10-26 22:59:10 -0700

    threads

---
+++ dxcjava/src/Threads/Runtime1.java
@@ -0,0 +1,10 @@
+package Threads;
+
+public class Runtime1 {
+  
+	public static void main(String args[])throws Exception{  
+		Runtime.getRuntime().exec("mspaint");//will open a new notepad  
+	}  
+}  
+
+
