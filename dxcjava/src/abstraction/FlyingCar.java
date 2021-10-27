65a6f8498b652ab2d78ac43841dfeed573ea2799
sjammala <sjammala@DESKTOP-LGOLIJ3>
2021-10-27 09:05:30 -0700

    abstraction

---
+++ dxcjava/src/abstraction/FlyingCar.java
@@ -0,0 +1,25 @@
+package abstraction;
+
+public class FlyingCar {
+
+
+		// TODO Auto-generated method stub
+		public void stopCar() {
+			// TODO Auto-generated method stub
+
+		}
+
+		public void flyCar() {
+			// TODO Auto-generated method stub
+
+		}
+
+		public void landCar() {
+			// TODO Auto-generated method stub
+
+		}
+
+	
+	}
+
+
