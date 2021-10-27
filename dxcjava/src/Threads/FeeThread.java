f37269e4012c35c7d2e119585273171976bd1270
sjammala <sjammala@DESKTOP-LGOLIJ3>
2021-10-27 05:15:26 -0700

    feethead

---
+++ dxcjava/src/Threads/FeeThread.java
@@ -0,0 +1,15 @@
+package Threads;
+
+/**
+ * person standing in the fee que with my fee form
+ * @author Admin
+ *
+ */
+public class FeeThread extends Thread{
+
+	@Override
+	public void run() {
+		System.out.println("got fee noc");
+	}
+
+}
