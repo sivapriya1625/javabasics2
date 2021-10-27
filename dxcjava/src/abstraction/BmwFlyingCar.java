7368f7451a7b2b125f11c2576a3587b2d9ebec93
sjammala <sjammala@DESKTOP-LGOLIJ3>
2021-10-27 09:15:49 -0700

    Abstraction

--- dxcjava/src/abstraction/BmwFlyingCar.java
+++ dxcjava/src/abstraction/BmwFlyingCar.java
@@ -1,5 +1,25 @@
 package abstraction;
 
-public class BmwFlyingCar {
+public class BmwFlyingCar extends FlyingCar {
+	
+	@Override
+	public void stopCar() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void flyCar() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void landCar() {
+		// TODO Auto-generated method stub
+
+	}
 
 }
+
+
