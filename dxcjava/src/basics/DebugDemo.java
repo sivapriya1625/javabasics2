04e806906688a438e3c7b761a40979e0675c5d7e
sjammala <sjammala@DESKTOP-LGOLIJ3>
2021-10-27 21:40:31 -0700

    method in java

---
+++ dxcjava/src/basics/DebugDemo.java
@@ -0,0 +1,23 @@
+package basics;
+
+public class DebugDemo {
+	 public static void main(String[] args) {
+			String sum = null;
+			System.out.println("the sum is--"+sum);
+
+			for(int a=0; a<5; a++) {
+				int f = a+ 5 *3;
+				//System.out.println("the no is="+f);
+				int j = a *2;
+				int f1 = a+ 5 *3 +j;
+				System.out.println("the no is="+f1);
+
+			}
+			//throw new NullPointerException();
+			throw new NullPointerException();
+
+		}
+
+	}
+
+
