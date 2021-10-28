52c1fc5579086f6c3c80f5609dfb8493152309a1
sjammala <sjammala@DESKTOP-LGOLIJ3>
2021-10-27 21:30:02 -0700

    constructors

--- dxcjava/src/basics/Student.java
+++ dxcjava/src/basics/Student.java
@@ -1,5 +1,30 @@
 package basics;
 
-public class Student {
 
+/**
+ * default constructor -- harshita
+ */
+
+public class Student {
+	private String name;
+	private int age;
+
+	/**
+	 * lahari constructor
+	 * @param name
+	 */
+	public Student(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * this is sathvika constructor
+	 * @param name
+	 * @param age
+	 */
+	public Student(String name, int age) {
+		super();
+		this.name = name;
+		this.age = age+2;
+	}
 }
---
+++ dxcjava/src/objectclass/Student.java
@@ -0,0 +1,5 @@
+package objectclass;
+
+public class Student {
+
+}
