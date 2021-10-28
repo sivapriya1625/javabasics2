6d796f64a0e37e0a2ba1a48773e71e5ae49212bf
sjammala <sjammala@DESKTOP-LGOLIJ3>
2021-10-27 21:49:01 -0700

    adding a watch

--- dxcjava/src/basics/DebugDemo.java
+++ dxcjava/src/basics/DebugDemo.java
@@ -1,23 +1,12 @@
 package basics;
 
 public class DebugDemo {
-	 public static void main(String[] args) {
-			String sum = null;
-			System.out.println("the sum is--"+sum);
+	public static void main(String[] args) {
+		Calculator calc = new Calculator();
+		int sum = calc.add(10, 20);
+		System.out.println("the sum is--"+sum);
 
-			for(int a=0; a<5; a++) {
-				int f = a+ 5 *3;
-				//System.out.println("the no is="+f);
-				int j = a *2;
-				int f1 = a+ 5 *3 +j;
-				System.out.println("the no is="+f1);
-
-			}
-			//throw new NullPointerException();
-			throw new NullPointerException();
-
-		}
-
+		throw new NullPointerException();
 	}
 
-
+}
