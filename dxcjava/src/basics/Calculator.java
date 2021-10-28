0c7f73e7e9c57430914c7e3cff5b87cac7881e07
sjammala <sjammala@DESKTOP-LGOLIJ3>
2021-10-27 21:38:28 -0700

    method in java

---
+++ dxcjava/src/basics/Calculator.java
@@ -0,0 +1,15 @@
+package basics;
+
+public class Calculator {
+	 int add(int a, int b) {
+
+			int c = a+b;
+			System.out.println("Calculator-- add--"+c);
+
+			return c;
+
+		}
+
+		void sub(int a, int b) { 
+}
+}
