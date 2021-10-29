770e1956fb77a4e9b425d3f4623db85211c91273
sjammala <sjammala@DESKTOP-LGOLIJ3>
2021-10-29 00:51:51 -0700

    streams

---
+++ dxcjava/src/streams/FileOutputStreamExample.java
@@ -0,0 +1,24 @@
+package streams;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class FileOutputStreamExample {
+
+	public static void main(String[] args) {
+
+        try {
+			FileOutputStream foutPipe = new FileOutputStream("C:\\Users\\Admin\\Desktop\\testout.txt"); // \\ is escape sequence
+            foutPipe.write(65);    
+            foutPipe.close();   //closing the pipe
+            System.out.println("success...");    
+
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}//   \\ is escape sequence \"    
+
+	}
+
+}
