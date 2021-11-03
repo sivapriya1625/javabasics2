b2fd11fb257a89c4a0b5bf102fe9f299e749998b
sjammala <sjammala@DESKTOP-LGOLIJ3>
2021-11-02 19:58:42 -0700

    dependency injection

---
+++ dxcjava/src/design/patterns/factory/Demosticplan.java
@@ -0,0 +1,11 @@
+package design.patterns.factory;
+
+class  DomesticPlan extends Plan{  
+    private double rate;
+
+	//@override  
+     public void getRate(){  
+         rate=3.50;              
+    }  
+}//end of DomesticPlan class.
+
---
+++ dxcjava/src/design/patterns/factory/InstitutionalPlan.java
@@ -0,0 +1,10 @@
+package design.patterns.factory;
+
+class  InstitutionalPlan extends Plan{  
+   //@override  
+    public void getRate(){   
+        rate=5.50;  
+   }   
+//end of InstitutionalPlan class.
+
+}
